40efb77339398ee2c8f9cbe6a96c36e541aacb49
kayz <kayz@pop-os.localdomain>
2022-12-06 15:00:36 +0100

    is_integral finished

--- inc/iterator_traits.hpp
+++ inc/iterator_traits.hpp
@@ -3,7 +3,69 @@
 
 namespace ft
 {
+    /**
+     * * Iterator base class:
+     * This is a base class template that can be used to derive iterator classes from it.
+     * It is not an iterator class and does not provide any of the functionality an iterator is expected to have.
+     * https://cplusplus.com/reference/iterator/iterator/
+     */
+    class input_iterator_tag {};
+    class output_iterator_tag {};
+    class forward_iterator_tag {};
+    class bidirectional_iterator_tag {};
+    class random_access_iterator_tag {};
+    /**
+     * * Iterator traits:
+     * Standard algorithms determine certain properties of the iterators passed to them and the range they represent,
+     *  by using the members of the corresponding iterator_traits instantiation.
+     * https://cplusplus.com/reference/iterator/iterator_traits/
+     */
+    template <class Iterator>
+    class iterator_traits
+    {
+        typedef typename Iterator::difference_type      difference_type;
+        typedef typename Iterator::value_type           value_type;
+        typedef typename Iterator::pointer              pointer;
+        typedef typename Iterator::reference            reference;
+        typedef typename Iterator::iterator_category    iterator_category;
+    };
 
+    template <class T>
+    class iterator_traits<T *>
+    {
+        typedef ptrdiff_t                               difference_type;
+        typedef T                                       value_type;
+        typedef T*                                      pointer;
+        typedef T&                                      reference;
+        typedef ft::random_access_iterator_tag          iterator_category;
+    };
+
+    template <class T>
+    class iterator_traits<const T *>
+    {
+        typedef ptrdiff_t                               difference_type;
+        typedef T                                       value_type;
+        typedef const T*                                pointer;
+        typedef const T&                                reference;
+        typedef ft::random_access_iterator_tag          iterator_category;
+    };
+    /**
+     * * Predefined iterators:
+     * https://cplusplus.com/reference/iterator/
+    */
+   template<class InputIterator>
+        typename ft::iterator_traits<InputIterator>::difference_type
+            distance (InputIterator first, InputIterator last)
+        {
+            typename ft::iterator_traits<InputIterator>::difference_type rtn = 0;
+            while (first != last)
+            {
+                first++;
+                rtn++;
+            }
+            return (rtn);
+        }
+    template <class Iterator> class reverse_iterator;
 }
 
 #endif
--- inc/vector.hpp
+++ inc/vector.hpp
@@ -5,242 +5,240 @@
 
 namespace ft
 {
-    template <class T, class Alloc = std::allocator<T>>
-    class vector
-    {
-        /**
-        * * MEMBER TYPES:
-        https://en.cppreference.com/w/cpp/container/vector
-        */
-       public:
-        // value_type       T
-        typedef T value_type;
-        // allocator_type   Alloctor
-        typedef Alloc allocator_type;
-        // size_type        std::size_t
-        typedef std::size_t size_type;
-        // difference_type  std::ptrdiff_t
-        typedef std::ptrdiff_t difference_type;
-        // reference        value_type&
-        // const_reference  const value_type&
-        // pointer          Allocator::pointer
-        // const_pointer    Allocator::const_pointer
-        // iterator
-        // const_iterator
-        // reverse_iterator
-        // const_reverse_iterator
+        template <class T, class Alloc = std::allocator<T> >
+        class vector
+        {
+                /**
+                * * MEMBER TYPES:
+                https://en.cppreference.com/w/cpp/container/vector
+                */
+        public:
+                // value_type       T
+                typedef T value_type;
+                // allocator_type   Alloctor
+                typedef Alloc allocator_type;
+                // size_type        std::size_t
+                typedef std::size_t size_type;
+                // difference_type  std::ptrdiff_t
+                typedef std::ptrdiff_t difference_type;
+                // reference        value_type&
+                // const_reference  const value_type&
+                // pointer          Allocator::pointer
+                // const_pointer    Allocator::const_pointer
+                // iterator
+                // const_iterator
+                // reverse_iterator
+                // const_reverse_iterator
 
-        /**
-         * * MEMBER FUNCTIONS:
-        https://en.cppreference.com/w/cpp/container/vector/vector
-         * ? constructors
-         */
-        // vector();
-        // explicit vector(const Allocator &alloc);
+                /**
+                 * * MEMBER FUNCTIONS:
+                https://en.cppreference.com/w/cpp/container/vector/vector
+                 * ? constructors
+                 */
+                // vector();
+                // explicit vector(const Allocator &alloc);
 
-        // explicit vector(size_type count,
-        //                 const T &value = T(),
-        //                 const Allocator &alloc = Allocator());
+                // explicit vector(size_type count,
+                //                 const T &value = T(),
+                //                 const Allocator &alloc = Allocator());
 
-        // template <class InputIt>
-        // vector(InputIt first, InputIt last,
-        //        const Allocator &alloc = Allocator());
+                // template <class InputIt>
+                // vector(InputIt first, InputIt last,
+                //        const Allocator &alloc = Allocator());
 
-        // vector(const vector &other);
+                // vector(const vector &other);
 
-        /**
-         * ? destructor
-         */
-        // ~vector();
+                /**
+                 * ? destructor
+                 */
+                // ~vector();
 
-        /**
-         * ? operator=
-         */
-        // vector &operator=(const vector &other);
+                /**
+                 * ? operator=
+                 */
+                // vector &operator=(const vector &other);
 
-        /**
-         * ? assign
-         */
-        // std::vector<T, Allocator>::assign
-        // template <class InputIt>
-        // void assign(InputIt first, InputIt last);
+                /**
+                 * ? assign
+                 */
+                // std::vector<T, Allocator>::assign
+                // template <class InputIt>
+                // void assign(InputIt first, InputIt last);
 
-        /**
-         * ? get_allocator
-         */
-        // allocator_type get_allocator() const;
+                /**
+                 * ? get_allocator
+                 */
+                // allocator_type get_allocator() const;
 
-        /**
-         * ? at
-         */
-        // reference at(size_type pos);
+                /**
+                 * ? at
+                 */
+                // reference at(size_type pos);
 
-        // const_reference at( size_type pos ) const;
+                // const_reference at( size_type pos ) const;
 
-        /**
-         * ? operator []
-         */
+                /**
+                 * ? operator []
+                 */
 
-        // reference operator[]( size_type pos );
+                // reference operator[]( size_type pos );
 
-        // const_reference operator[]( size_type pos ) const;
+                // const_reference operator[]( size_type pos ) const;
 
-        /**
-         * ? front
-         */
-        // reference front();
+                /**
+                 * ? front
+                 */
+                // reference front();
 
-        // const_reference front() const;
+                // const_reference front() const;
 
-        /**
-         * ? back
-         */
-        // reference back();
+                /**
+                 * ? back
+                 */
+                // reference back();
 
-        // const_reference back() const;
+                // const_reference back() const;
 
-        /**
-         * ? data
-         */
+                /**
+                 * ? data
+                 */
 
-        // T* data();
+                // T* data();
 
-        // const T *data() const;
+                // const T *data() const;
 
-        /**
-         * ? begin
-         */
+                /**
+                 * ? begin
+                 */
 
-        // iterator begin();
+                // iterator begin();
 
-        // const_iterator begin() const;
+                // const_iterator begin() const;
 
+                /**
+                 * ? end
+                 */
+                // iterator end();
 
-        /**
-         * ? end
-         */
-        // iterator end();
+                // const_iterator end() const;
 
-        // const_iterator end() const;
+                /**
+                 * ? rbegin
+                 */
+                // reverse_iterator rbegin();
 
-        /**
-         * ? rbegin
-         */
-        // reverse_iterator rbegin();
+                // const_reverse_iterator rbegin() const;
 
-        // const_reverse_iterator rbegin() const;
+                /**
+                 * ? rend
+                 */
+                // reverse_iterator rend();
 
-        /**
-         * ? rend
-         */
-        // reverse_iterator rend();
+                // const_reverse_iterator rend() const;
 
-        // const_reverse_iterator rend() const;
+                /**
+                 * ? empty
+                 */
+                // bool empty() const;
 
-        /**
-         * ? empty
-         */
-        // bool empty() const;
+                /**
+                 * ? size
+                 */
+                // size_type size() const;
 
-        /**
-         * ? size
-         */
-        // size_type size() const;
+                /**
+                 * ? max_size
+                 */
+                // size_type max_size() const;
+                /**
+                 * ? reserve
+                 */
+                // void reserve( size_type new_cap );
 
-        /**
-         * ? max_size
-         */
-        // size_type max_size() const;
-        /**
-         * ? reserve
-         */
-        // void reserve( size_type new_cap );
+                /**
+                 * ? capacity
+                 */
+                // size_type capacity() const;
 
-        /**
-         * ? capacity
-         */
-        // size_type capacity() const;
+                /**
+                 * ? clear
+                 */
+                // void clear();
 
-        /**
-         * ? clear
-         */
-        // void clear();
+                /**
+                 * ? insert
+                 */
+                // iterator insert(const_iterator pos, const T &value);
 
-        /**
-         * ? insert
-         */
-        // iterator insert(const_iterator pos, const T &value);
+                // iterator insert(const_iterator pos, size_type count, const T &value);
 
-        // iterator insert(const_iterator pos, size_type count, const T &value);
+                // template< class InputIt >
+                // iterator insert(const_iterator pos, InputIt first, InputIt last);
 
-        // template< class InputIt >
-        // iterator insert(const_iterator pos, InputIt first, InputIt last);
+                /**
+                 * ? erase
+                 */
+                // iterator erase(iterator pos);
 
+                // iterator erase( iterator first, iterator last );
 
-        /**
-         * ? erase
-         */
-        // iterator erase(iterator pos);
+                /**
+                 * ? push_back
+                 */
+                // void push_back( const T& value );
 
-        // iterator erase( iterator first, iterator last );
+                //
+                /**
+                 * ? pop_back
+                 */
+                // void pop_back();
 
-        /**
-         * ? push_back
-         */
-        // void push_back( const T& value );
+                /**
+                 * ? resize
+                 */
+                // void resize( size_type count, T value = T() );
 
-        // 
-        /**
-         * ? pop_back
-         */
-        // void pop_back();
+                /**
+                 * ? swap
+                 */
+                // void swap(vector &other);
 
-        /**
-         * ? resize
-         */
-        // void resize( size_type count, T value = T() );
-        
-        /**
-         * ? swap
-         */
-        // void swap(vector &other);
+                // NON-MEMBER FUNCITONS
 
-        // NON-MEMBER FUNCITONS
-        
-        /**
-         * ? operator==
-         */
-        // bool operator==( const std::vector<T,Alloc>& lhs,
+                /**
+                 * ? operator==
+                 */
+                // bool operator==( const std::vector<T,Alloc>& lhs,
                 //  const std::vector<T,Alloc>& rhs );
-        /**
-         * ? operator!=
-         */
-        // bool operator!=( const std::vector<T,Alloc>& lhs,
+                /**
+                 * ? operator!=
+                 */
+                // bool operator!=( const std::vector<T,Alloc>& lhs,
                 //  const std::vector<T,Alloc>& rhs );
 
-        /**
-         * ? operator<
-         */
-        // bool operator<(const std::vector<T, Alloc> &lhs,
-                    //    const std::vector<T, Alloc> &rhs);
+                /**
+                 * ? operator<
+                 */
+                // bool operator<(const std::vector<T, Alloc> &lhs,
+                //    const std::vector<T, Alloc> &rhs);
 
-        /**
-         * ? operator<=
-         */
-        // bool operator<=( const std::vector<T,Alloc>& lhs,
+                /**
+                 * ? operator<=
+                 */
+                // bool operator<=( const std::vector<T,Alloc>& lhs,
                 //  const std::vector<T,Alloc>& rhs );
 
-        /**
-         * ? operator>
-         */
-        // bool operator>( const std::vector<T,Alloc>& lhs,
+                /**
+                 * ? operator>
+                 */
+                // bool operator>( const std::vector<T,Alloc>& lhs,
                 // const std::vector<T,Alloc>& rhs );
 
-        /**
-         * ? operator>=
-         */
-        // bool operator>=(const std::vector<T, Alloc> &lhs,
-                        // const std::vector<T, Alloc> &rhs);
+                /**
+                 * ? operator>=
+                 */
+                // bool operator>=(const std::vector<T, Alloc> &lhs,
+                // const std::vector<T, Alloc> &rhs);
         }
 
 };
